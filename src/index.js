293729420627758dc2ff56728a3d86a829db5e78
adrianatwatanabe <adriana.t.watanabe@gmail.com>
2022-06-02 23:48:59 -0300

    Adicionando rotas privadas

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import PrivateRoute from './components/PrivateComponent';
+
 import Login from './pages/Login';
 import Management from './pages/Management';
 import Register from './pages/Register';
@@ -14,14 +16,22 @@ root.render(
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
-        <Route exact path='/' element={<Login />} />
-        <Route exact path='menu' element={<Management />} />
-        <Route exact path='register' element={<Register />} />
-        <Route exact path='employee' element={<UsersList />} />
-        <Route exact path='orders-progress' element={<OrdersProgress />} />
-        <Route exact path='orders' element={<Orders />} />
-        <Route exact path='delivery' element={<OrdersDelivered />} />
-        <Route exact path='finish' element={<Orders />} />
+        <Route exact path='/' end element={<Login />}/>
+          <Route exact path='menu' element={
+            <PrivateRoute>
+              <Management />
+            </PrivateRoute>
+          } />
+          <Route exact path='register' element={
+            <PrivateRoute>
+              <Register />
+            </PrivateRoute>
+          } />
+          <Route exact path='employee' element={<UsersList />} />
+          <Route exact path='orders-progress' element={<OrdersProgress />} />
+          <Route exact path='orders' element={<Orders />} />
+          <Route exact path='delivery' element={<OrdersDelivered />} />
+          <Route exact path='finish' element={<Orders />} />
       </Routes>
     </BrowserRouter>
   </React.StrictMode>
